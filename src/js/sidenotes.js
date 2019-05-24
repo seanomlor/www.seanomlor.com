98fe12001fff1297c24a634bbc8a21157356bb5a
Sean Omlor <somlor@eml.cc>
2019-05-24 15:41:20 -0400

    narrow column, remove avatar from sidebar

--- src/js/sidenotes.js
+++ src/js/sidenotes.js
@@ -110,7 +110,7 @@ const createInsertSidenote = () => {
 const sidenotes = () => {
   window.onload = () => {
     let done = false
-    addMediaQueryListener('(min-width: 1420px)', _e => {
+    addMediaQueryListener('(min-width: 1280px)', _e => {
       if (!done) {
         const insertSideNote = createInsertSidenote()
         document.querySelectorAll('sup.footnote-ref a').forEach(insertSideNote)
